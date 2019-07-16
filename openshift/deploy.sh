97d0196738bc5a5a7374b43f9fdb6a3fd22cd8a3
Marc van Dijk <marcvdijk@gmail.com>
2019-07-16 15:07:21 +0200

    OpenShift deployment template fixes

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 source setenv.sh
 
-oc process -f os-mdstudio_smartcyp-template.yaml -p ROUTES_BASENAME=$OC_ROUTES_BASENAME | oc create -f -
+oc process -f os-mdstudio-smartcyp-template.yaml -p ROUTES_BASENAME=$OC_ROUTES_BASENAME | oc create -f -
