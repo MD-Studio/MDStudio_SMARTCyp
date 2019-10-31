2a65e71b5f389b605dd7dc7a7d824889be6de43e
Marc van Dijk <marcvdijk@gmail.com>
2019-10-31 14:20:31 +0100

    Fix tmp dir path

--- entry_point_mdstudio_smartcyp.sh
+++ entry_point_mdstudio_smartcyp.sh
@@ -3,4 +3,4 @@
 export MD_CONFIG_ENVIRONMENTS=dev,docker
 
 # Start REST server
-python -u -m mdstudio_smartcyp -a rest -w /tmp -r 1
+python -u -m mdstudio_smartcyp -a rest -w /tmp/mdstudio/mdstudio_smartcyp -r 1
