b1a5e0c4d2279246002461533f4503ec3cab294d
Marc van Dijk <marcvdijk@gmail.com>
2019-10-31 14:18:57 +0100

    Fix all WAMP endpoints

--- mdstudio_smartcyp/wamp_services.py
+++ mdstudio_smartcyp/wamp_services.py
@@ -15,8 +15,8 @@ from mdstudio.component.session import ComponentSession
 from mdstudio_smartcyp.combined_prediction import CombinedPrediction
 from mdstudio_smartcyp.smartcyp_run import SmartCypRunner, smartcyp_version_info
 from mdstudio_smartcyp.plants_run import PlantsDocking, plants_version_info
-from mdstudio_smartcyp.spores_run import SporesRunner
-from mdstudio_smartcyp.utils import mol_validate_file_object
+from mdstudio_smartcyp.spores_run import spores_version_info, SporesRunner
+from mdstudio_smartcyp.utils import mol_validate_file_object, MDStudioException
 
 
 def encoder(file_path):
@@ -44,14 +44,19 @@ class SmartCypWampApi(ComponentSession):
 
         return True
 
-    @endpoint('smartcyp_info', 'info_request', 'info_response', options=RegisterOptions(invoke=u'roundrobin'))
+    @endpoint('smartcyp_info', 'smartcyp_info_request', 'smartcyp_info_response',
+              options=RegisterOptions(invoke=u'roundrobin'))
     def smartcyp_info(self, request, claims):
         """
         Returns an informative summary of the supported SMARTCyp version
         and configuration.
         """
 
-        return smartcyp_version_info()
+        info = smartcyp_version_info()
+        if info:
+            return {'status': 'completed', 'info': info}
+
+        return {'status': 'failed'}
 
     @endpoint('smartcyp', 'smartcyp_request', 'smartcyp_response', options=RegisterOptions(invoke=u'roundrobin'))
     def smartcyp_prediction(self, request, claims):
@@ -77,7 +82,139 @@ class SmartCypWampApi(ComponentSession):
         # Remove temporary working directory
         smartcyp.delete()
 
+        result_dict['status'] = 'completed' if result_dict['result'] is not None else 'failed'
         return result_dict
+
+    @endpoint('docking_info', 'docking_info_request', 'docking_info_response',
+              options=RegisterOptions(invoke=u'roundrobin'))
+    def plants_info(self, request, claims):
+        """
+        Returns an informative summary of the supported PLANTS version
+        and configuration.
+        """
+
+        info = plants_version_info()
+        if info:
+            return {'status': 'completed', 'info': info}
+
+        return {'status': 'failed'}
+
+    @endpoint('docking', 'docking_request', 'docking_response', options=RegisterOptions(invoke='roundrobin'))
+    def plants_docking(self, request, claims):
+        """
+        Perform a PLANTS (Protein-Ligand ANT System) molecular docking.
+        For a detail description of the input see the file:
+        schemas/endpoints/docking-request.v1.json
+        """
+
+        # Validate input path_file object for protein and ligand file
+        protein_file = mol_validate_file_object(request['protein_file'])
+        ligand_file = mol_validate_file_object(request['ligand_file'])
+
+        # Run docking
+        base_dir = os.environ.get('BASE_WORK_DIR', request.get('base_work_dir'))
+
+        for drop_key in ('protein_file', 'ligand_file', 'base_work_dir'):
+            if drop_key in request:
+                del request[drop_key]
+
+        docking = PlantsDocking(log=self.log, base_work_dir=base_dir, **request)
+
+        if docking.run(protein_file['content'], ligand_file['content']):
+            return {'status': 'completed', 'result': docking.get_results()}
+
+        self.log.error('PLANTS docking failed')
+        return {'status': 'failed'}
+
+    @endpoint('docking_statistics', 'docking_statistics_request', 'docking_statistics_response',
+              options=RegisterOptions(invoke='roundrobin'))
+    def plants_docking_statistics(self, request, claims):
+        """
+        Return PLANTS docking statistics for particular docking solutions run previously.
+        Clustering will also be redone and optionally adjusted.
+        """
+
+        base_dir = os.environ.get('BASE_WORK_DIR', request.get('base_work_dir'))
+        if 'base_work_dir' in request:
+            del request['base_work_dir']
+
+        docking = PlantsDocking(log=self.log, base_work_dir=base_dir, **request)
+
+        try:
+            results = docking.get_results(structures=request.get('paths'))
+        except MDStudioException as error:
+            self.log.error(repr(error))
+            return {'status': 'failed'}
+
+        if results:
+            return {'status': 'completed', 'result': results}
+
+        self.log.error('PLANTS docking failed')
+        return {'status': 'failed'}
+
+    @endpoint('docking_structures', 'docking_structures_request', 'docking_structures_response',
+              options=RegisterOptions(invoke='roundrobin'))
+    def plants_docking_structures(self, request, claims):
+        """
+        Return PLANTS docked structures
+        """
+
+        base_dir = os.environ.get('BASE_WORK_DIR', request.get('base_work_dir'))
+        docking = PlantsDocking(log=self.log, base_work_dir=base_dir)
+
+        try:
+            results = docking.get_structures(structures=request.get('paths'),
+                                             output_format=request.get('output_format', 'mol2'),
+                                             include_protein=request.get('include_protein', False),
+                                             create_ensemble=request.get('create_ensemble', True))
+        except MDStudioException as error:
+            self.log.error(repr(error))
+            return {'status': 'failed'}
+
+        # Encode files
+        if not isinstance(results, list):
+            results = [results]
+
+        output = [{"path": None, "extension": request.get('output_format', 'mol2'), "content": mol, "encoding": "utf8"}
+                  for mol in results]
+
+        return {'status': 'completed', 'result': output}
+
+    @endpoint('spores_info', 'spores_info_request', 'spores_info_response',
+              options=RegisterOptions(invoke=u'roundrobin'))
+    def spores_info(self, request, claims):
+        """
+        Returns an informative summary of the supported SPORES version
+        and configuration.
+        """
+
+        info = spores_version_info()
+        if info:
+            return {'status': 'completed', 'info': info}
+
+        return {'status': 'failed'}
+
+    @endpoint('spores', 'spores_request', 'spores_response', options=RegisterOptions(invoke='roundrobin'))
+    def spores_run(self, request, claims):
+        """
+        Perform a SPORES (Structure PrOtonation and REcognition System) structure preparation.
+        For a detail description of the input see the file:
+        schemas/endpoints/spores-request.v1.json
+        """
+
+        # Validate input path_file object for mol
+        mol = mol_validate_file_object(request['mol'])
+
+        spores = SporesRunner(log=self.log, base_work_dir=os.environ.get('BASE_WORK_DIR', request.get('base_work_dir')))
+        try:
+            result_dict = spores.run(mol['content'], mode=request['spores_mode'], input_format=request['input_format'])
+        except Exception as e:
+            self.log.error(str(e))
+            return {'status': 'failed'}
+        finally:
+            spores.delete()
+
+        return {'status': 'completed', 'result': result_dict}
 
     @endpoint('som_prediction', 'som_prediction_request', 'som_prediction_response',
               options=RegisterOptions(invoke='roundrobin'))
@@ -99,111 +236,3 @@ class SmartCypWampApi(ComponentSession):
 
         self.log.error('SOM prediction failed')
         return {'status': 'failed'}
-
-    @endpoint('docking_info', 'info_request', 'info_response', options=RegisterOptions(invoke=u'roundrobin'))
-    def plants_info(self, request, claims):
-        """
-        Returns an informative summary of the supported PLANTS version
-        and configuration.
-        """
-
-        return plants_version_info()
-
-    @endpoint('docking', 'docking_request', 'docking_response', options=RegisterOptions(invoke='roundrobin'))
-    def plants_docking(self, request, claims):
-        """
-        Perform a PLANTS (Protein-Ligand ANT System) molecular docking.
-        For a detail description of the input see the file:
-        schemas/endpoints/docking-request.v1.json
-        """
-
-        # Validate input path_file object for protein and ligand file
-        protein_file = mol_validate_file_object(request['protein_file'])
-        ligand_file = mol_validate_file_object(request['ligand_file'])
-
-        # Run docking
-        base_dir = os.environ.get('BASE_WORK_DIR', request.get('base_work_dir'))
-        docking = PlantsDocking(base_work_dir=base_dir)
-        success = docking.run(protein_file['content'], ligand_file['content'])
-
-        if success:
-            status = 'completed'
-            results = docking.get_results()
-            output = {key: encode_file(value) for key, value in results.items()}
-
-        else:
-            self.log.error('PLANTS docking failed')
-            return {'status': 'failed'}
-
-        return {'status': status, 'output': output}
-
-    @endpoint('docking_statistics', 'docking_statistics_request', 'docking_statistics_response',
-              options=RegisterOptions(invoke='roundrobin'))
-    def plants_docking_statistics(self, request, claims):
-        """
-        Return PLANTS docking statistics for particular docking solutions run previously.
-        Clustering will also be redone and optionally adjusted.
-        """
-
-        base_dir = os.environ.get('BASE_WORK_DIR', request.get('base_work_dir'))
-        base_path = list(set([os.path.dirname(p) for p in request['paths']]))
-        if len(base_path) > 1:
-            self.log.error('Unable to combine results for more then one docking run')
-            return {'status': 'failed'}
-
-        if not os.path.exists(os.path.join(base_dir or '', base_path[0])):
-            self.log.error('Docking results (no longer) exist: {0}'.format(os.path.basename(base_path[0])))
-            return {'status': 'failed'}
-
-        docking = PlantsDocking(base_work_dir=base_dir)
-        docking.workdir = base_path[0]
-        docking.update(**request)
-
-        results = docking.get_results(structures=request['paths'])
-        if results:
-            return {'status': 'completed', 'output': results}
-
-        self.log.error('PLANTS docking failed')
-        return {'status': 'failed'}
-
-    @endpoint('docking_structures', 'docking_structures_request', 'docking_structures_response',
-              options=RegisterOptions(invoke='roundrobin'))
-    def plants_docking_structures(self, request, claims):
-        """
-        Return PLANTS docking statistics for particular docking solutions run previously.
-        Clustering will also be redone and optionally adjusted.
-        """
-
-        base_dir = os.environ.get('BASE_WORK_DIR', request.get('base_work_dir'))
-        base_path = list(set([os.path.dirname(p) for p in request['paths']]))
-        if len(base_path) > 1:
-            self.log.error('Unable to combine results for more then one docking run')
-            return {'status': 'failed'}
-
-        if not os.path.exists(os.path.join(base_dir or '', base_path[0])):
-            self.log.error('Docking results (no longer) exist: {0}'.format(os.path.basename(base_path[0])))
-            return {'status': 'failed'}
-
-        docking = PlantsDocking(base_work_dir=base_dir)
-        docking.workdir = base_path[0]
-        results = docking.get_structures(**request)
-
-        return results
-
-    @endpoint('spores', 'spores_request', 'spores_response', options=RegisterOptions(invoke='roundrobin'))
-    def spores_run(self, request, claims):
-        """
-        Perform a SPORES (Structure PrOtonation and REcognition System) structure preparation.
-        For a detail description of the input see the file:
-        schemas/endpoints/spores-request.v1.json
-        """
-
-        # Validate input path_file object for mol
-        mol = mol_validate_file_object(request['mol'])
-
-        base_dir = os.environ.get('BASE_WORK_DIR', request.get('base_work_dir'))
-        spores = SporesRunner(log=self.log, base_work_dir=base_dir)
-        result_dict = spores.run(mol['content'], mode=request['spores_mode'], input_format=request['input_format'])
-        spores.delete()
-
-        return result_dict
