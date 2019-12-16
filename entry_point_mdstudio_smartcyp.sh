92ebd48af891188d509c23e297437218c00ec136
Marc van Dijk <marcvdijk@gmail.com>
2019-12-16 09:35:10 +0100

    Default to WAMP interface

--- entry_point_mdstudio_smartcyp.sh
+++ entry_point_mdstudio_smartcyp.sh
@@ -3,4 +3,4 @@
 export MD_CONFIG_ENVIRONMENTS=dev,docker
 
 # Start REST server
-python -u -m mdstudio_smartcyp -a rest -w /tmp/mdstudio/mdstudio_smartcyp -r 1
+python -u -m mdstudio_smartcyp -a wamp -w /tmp/mdstudio/mdstudio_smartcyp -r 1
